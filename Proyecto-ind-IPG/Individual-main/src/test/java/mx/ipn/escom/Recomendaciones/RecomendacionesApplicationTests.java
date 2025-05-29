cf9dda2202b55cde9f57a51186c899bfddcb70a6
IsaacPG11 <137459133+Isaac11PG@users.noreply.github.com>
2025-05-28 23:53:45 -0600

    Proyecto Individual
    Proyecto individual de libors y peliculas

---
+++ Proyecto-ind-IPG/Individual-main/src/test/java/mx/ipn/escom/Recomendaciones/RecomendacionesApplicationTests.java
@@ -0,0 +1,15 @@
+package mx.ipn.escom.Recomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import mx.ipn.escom.Recomendaciones.auth.RecomendacionesApplication;
+
+
+@SpringBootTest(classes = RecomendacionesApplication.class)
+public class RecomendacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Este test asegura que el contexto de Spring se carga correctamente
+    }
+}
